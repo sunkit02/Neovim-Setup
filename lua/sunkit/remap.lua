0a24a8f9bce9814fc0b1cba1796424f164e8bc94
Sun Kit Tsui <sunkittsui@gmail.com>
2023-03-04 01:42:09 -0600

    Added comments in remaps.lua

--- lua/sunkit/remap.lua
+++ lua/sunkit/remap.lua
@@ -1,19 +1,24 @@
 vim.g.mapleader = " "
 vim.keymap.set("i", "jk", "<ESC>")
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
+--moving highlighted lines
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+--keeping cursor centered after jumping up and down
+--the page and searching text
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+--keeping origin buffer content after pasting over
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
+--seperating vim clipboard and system clipboard
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
@@ -21,6 +26,7 @@ vim.keymap.set("n", "<leader>Y", "\"+Y")
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("n", "<leader>d", "\"_d")
 
+--quickfix navigations
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
