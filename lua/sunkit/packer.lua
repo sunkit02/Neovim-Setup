856af9ebea45f54598369ed491355bbd6ebc17ef
Sun Kit Tsui <sunkittsui@gmail.com>
2023-03-05 15:52:53 -0600

    Added Comment plugin

--- lua/sunkit/packer.lua
+++ lua/sunkit/packer.lua
@@ -2,7 +2,7 @@
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
-
+--
 return require('packer').startup(function(use)
 	-- Packer can manage itself
 	use 'wbthomason/packer.nvim'
@@ -50,4 +50,7 @@ return require('packer').startup(function(use)
 			{'rafamadriz/friendly-snippets'}, -- Optional
 		}
 	}
+
+    -- Comments
+    use('numToStr/Comment.nvim')
 end)
