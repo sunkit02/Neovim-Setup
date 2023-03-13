a41bbbb1b7e1b16226f4d9eeaf21bdd0640cb131
Sun Kit Tsui <sunkittsui@gmail.com>
2023-03-13 11:04:11 -0500

    Improved comments in remap.lua

--- lua/sunkit/remap.lua
+++ lua/sunkit/remap.lua
@@ -8,7 +8,7 @@ vim.keymap.set("n", "<leader>e",  ":Lex 30<cr>")
 -- Easy sourcing of config files
 vim.keymap.set("n", "<leader><CR>", ":so %<CR>")
 
---jump cursor to new window after creating it
+-- Jump cursor to new window after creating it
 vim.keymap.set("n", "<C-w>v", "<C-w>v<C-w>l")
 vim.keymap.set("n", "<C-w>s", "<C-w>s<C-w>j")
 
@@ -18,36 +18,37 @@ vim.keymap.set("n", "<C-Down>", ":resize +2<CR>")
 vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>")
 vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>")
 
---moving highlighted lines
+-- Moving highlighted lines
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
---keeping cursor centered after jumping up and down
---the page and searching text
+-- Keeping cursor centered after jumping up and down
+-- the page and searching text
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
---keeping origin buffer content after pasting over
+-- Keeping origin buffer content after pasting over
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
---seperating vim clipboard and system clipboard
+-- Seperating vim clipboard and system clipboard
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
 
 vim.keymap.set("n", "<leader>d", "\"_d")
 
---quickfix navigations
+-- Quickfix navigations
 vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>cprev<CR>zz")
 
---start typing to replace work currently under cursor
+-- Start typing to replace work currently under cursor
 vim.keymap.set("n", "<leader>r", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+-- Make current file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- Better tab navigation
