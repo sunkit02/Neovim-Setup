4df0a358e6f9cca2d50e0e5cae6257be02e3e474
Sun Kit Tsui <sunkittsui@gmail.com>
2023-03-14 09:49:55 -0500

    Added rose-pine to be downloaded

--- lua/sunkit/packer.lua
+++ lua/sunkit/packer.lua
@@ -12,6 +12,7 @@ return require('packer').startup(function(use)
         -- or                            , branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
+    use ('rose-pine/neovim')
     -- use({
     --     'rose-pine/neovim',
     --     as = 'rose-pine',
