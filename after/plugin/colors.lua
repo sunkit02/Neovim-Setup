c968d288e7ded52219e530368fa53c04aecabd9f
Sun Kit Tsui <sunkittsui@gmail.com>
2023-04-20 10:03:59 -0500

    Clear bg for SignColumn

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,10 +1,19 @@
+local vim = vim
+
 function ColorMyPencils(color)
 	-- color = color or "rose-pine"
     color = color or "gruvbox"
 	vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+
+    -- vim.api.nvim_set_hl(0, "DiagnosticError", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "DiagnosticSignError", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "DiagnosticSignHint", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "DiagnosticSignInfo", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "DiagnosticSignWarn", { bg = "none" })
 end
 
 ColorMyPencils()
