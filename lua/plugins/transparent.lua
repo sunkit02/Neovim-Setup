6842025ad443129e5b8fce86d52532b7e2b124df
Sun Kit Tsui <sunkittsui@gmail.com>
2024-02-02 17:07:04 -0600

    Set colors for floating windows

--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -2,14 +2,12 @@ return {
   "xiyaowong/nvim-transparent",
   opts = {
     groups = { -- table: default groups
-      'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
-      'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
-      'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
-      'SigColumn', 'SignColumn', 'CursorLineNr', 'EndOfBuffer',
+      'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier', 'Statement', 'PreProc', 'Type', 'Underlined',
+      'Todo', 'String', 'Function', 'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText', 'SigColumn',
+      'SignColumn', 'CursorLineNr', 'EndOfBuffer', 'TelescopeNormal', 'TelescopeBorder', 'NvimTreeNormal',
+      "EndOfBuffer", "FloatBorder"
     },
-    extra_groups = {
-      'TelescopeNormal', 'TelescopeBorder', 'NvimTreeNormal'
-    },                   -- table: additional groups that should be cleared
+    extra_groups = {},   -- table: additional groups that should be cleared
     exclude_groups = {}, -- table: groups you don't want to clear
   }
 }
--- lua/settings/set_theme.lua
+++ lua/settings/set_theme.lua
@@ -2,10 +2,10 @@ function SetTheme(color)
   color = color or "tokyonight"
   vim.cmd.colorscheme(color)
 
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-    vim.api.nvim_set_hl(0, "VertSplit", { ctermbg = "none" })
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#222433" })
+  vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+  vim.api.nvim_set_hl(0, "VertSplit", { ctermbg = "none" })
 end
 
 SetTheme()
