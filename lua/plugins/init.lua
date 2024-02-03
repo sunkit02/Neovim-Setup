08ac0b0598ea55e51f03a25912fd0f282bbb5dbc
Sun Kit Tsui <sunkittsui@gmail.com>
2024-02-03 00:39:14 -0600

    Add lazy loading on *.md files for MarkdownPreview

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -60,6 +60,7 @@ return {
 
   {
     "iamcco/markdown-preview.nvim",
+    event = "BufEnter *.md",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
     build = function() vim.fn["mkdp#util#install"]() end,
