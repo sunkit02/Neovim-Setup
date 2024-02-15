2f8f437bb4395aa898167e741669d9981d32da26
Sun Kit Tsui <sunkittsui@gmail.com>
2024-02-15 15:02:45 -0600

    Add keybindings for common git operations

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,6 +1,11 @@
 return {
-	"tpope/vim-fugitive",
+  "tpope/vim-fugitive",
+  lazy = false,
   keys = {
-    { "<leader>gs", vim.cmd.Git , desc = "Open fugitive"}
+    { "<leader>gs",  vim.cmd.Git,          desc = "Open fugitive" },
+    { "<leader>gl",  "<cmd>Git log<CR>",   desc = "Open [G]it [l]ogs" },
+    { "<leader>gp",  "<cmd>Git push<CR>",  desc = "Open [G]it [p]ush" },
+    { "<leader>gP",  "<cmd>Git pull<CR>",  desc = "Open [G]it [p]ull" },
+    { "<leader>gaa", "<cmd>Git add .<CR>", desc = "Open [G]it [a]ll" },
   }
 }
