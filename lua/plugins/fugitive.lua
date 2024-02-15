44a5561b3b6e030daba9e0b59037c124d3be5491
Sun Kit Tsui <sunkittsui@gmail.com>
2024-02-15 15:17:40 -0600

    Add keybindings for common git operations

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,10 +2,12 @@ return {
   "tpope/vim-fugitive",
   lazy = false,
   keys = {
-    { "<leader>gs",  vim.cmd.Git,          desc = "Open fugitive" },
-    { "<leader>gl",  "<cmd>Git log<CR>",   desc = "Open [G]it [l]ogs" },
-    { "<leader>gp",  "<cmd>Git push<CR>",  desc = "Open [G]it [p]ush" },
-    { "<leader>gP",  "<cmd>Git pull<CR>",  desc = "Open [G]it [p]ull" },
-    { "<leader>gaa", "<cmd>Git add .<CR>", desc = "Open [G]it [a]ll" },
+    { "<leader>gs",  vim.cmd.Git,                   desc = "Open fugitive / [G]it [s]tatus" },
+    { "<leader>gl",  "<cmd>Git log<CR>",            desc = "Open [G]it [l]ogs" },
+    { "<leader>gp",  "<cmd>Git push<CR>",           desc = "Open [G]it [p]ush" },
+    { "<leader>gP",  "<cmd>Git pull<CR>",           desc = "Open [G]it [p]ull" },
+    { "<leader>gaa", "<cmd>Git add .<CR>",          desc = "Open [G]it [a]ll" },
+    { "<leader>gcc", "<cmd>Git commit<CR>",         desc = "Open [G]it [c]ommit" },
+    { "<leader>gca", "<cmd>Git commit --amend<CR>", desc = "Open [G]it [c]ommit [a]mend" },
   }
 }
