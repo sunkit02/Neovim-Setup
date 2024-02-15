ffe891c6c216e79e92e5b8509f3d3c761c0e8f0d
Sun Kit Tsui <sunkittsui@gmail.com>
2024-02-15 15:16:39 -0600

    Disable harpoon file nav keys

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,11 +1,11 @@
 return {
   "theprimeagen/harpoon",
   keys = {
-    { "<leader>m", function() require("harpoon.mark").add_file() end },
-    { "<C-e>", function() require("harpoon.ui").toggle_quick_menu() end },
-    { "<C-1>", function() require("harpoon.ui").nav_file(1) end },
-    { "<C-2>", function() require("harpoon.ui").nav_file(2) end },
-    { "<C-3>", function() require("harpoon.ui").nav_file(3) end },
-    { "<C-4>", function() require("harpoon.ui").nav_file(4) end },
+    { "<leader>m", function() require("harpoon.mark").add_file() end,        desc = "[M]ark file for harpoon." },
+    { "<C-e>",     function() require("harpoon.ui").toggle_quick_menu() end, desc = "Show harpoon list." },
+    -- { "<leader>A", function() require("harpoon.ui").nav_file(1) end,         desc = "Go to harpoon file 1." },
+    -- { "<leader>S", function() require("harpoon.ui").nav_file(2) end,         desc = "Go to harpoon file 2." },
+    -- { "<leader>D", function() require("harpoon.ui").nav_file(3) end,         desc = "Go to harpoon file 3." },
+    -- { "<leader>F", function() require("harpoon.ui").nav_file(4) end,         desc = "Go to harpoon file 4." },
   }
 }
