8df7b110f411cdd1d5c71f268b711436ef1a0ada
Sun Kit Tsui <sunkittsui@gmail.com>
2025-02-24 15:58:38 -0500

    Update harpoon keybindings

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -3,9 +3,9 @@ return {
   keys = {
     { "<leader>m", function() require("harpoon.mark").add_file() end,        desc = "[M]ark file for harpoon." },
     { "<C-e>",     function() require("harpoon.ui").toggle_quick_menu() end, desc = "Show harpoon list." },
-    -- { "<leader>A", function() require("harpoon.ui").nav_file(1) end,         desc = "Go to harpoon file 1." },
-    -- { "<leader>S", function() require("harpoon.ui").nav_file(2) end,         desc = "Go to harpoon file 2." },
-    -- { "<leader>D", function() require("harpoon.ui").nav_file(3) end,         desc = "Go to harpoon file 3." },
-    -- { "<leader>F", function() require("harpoon.ui").nav_file(4) end,         desc = "Go to harpoon file 4." },
+    { "<leader>z", function() require("harpoon.ui").nav_file(1) end,         desc = "Go to harpoon file 1." },
+    { "<leader>x", function() require("harpoon.ui").nav_file(2) end,         desc = "Go to harpoon file 2." },
+    { "<leader>c", function() require("harpoon.ui").nav_file(3) end,         desc = "Go to harpoon file 3." },
+    { "<leader>v", function() require("harpoon.ui").nav_file(4) end,         desc = "Go to harpoon file 4." },
   }
 }
