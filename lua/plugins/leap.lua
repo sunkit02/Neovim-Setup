18a3d5b4502a3998c5c5bde4b16ef00a58301810
Sun Kit Tsui <sunkittsui@gmail.com>
2025-04-08 03:28:49 -0400

    Add leap plugin for quick navigation

---
+++ lua/plugins/leap.lua
@@ -0,0 +1,10 @@
+return {
+  "ggandor/leap.nvim",
+  dependencies = {
+    "tpope/vim-repeat"
+  },
+  config = function()
+    local leap = require("leap")
+    leap.create_default_mappings()
+  end
+}
